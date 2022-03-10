774ba0c95899b2b9ae237751e20ccbd9d71f79e5
mameMan <jokingbear01@gmail.com>
2022-03-10 10:43:58 +0900

    connect to mongoDB

---
+++ boiler_plate/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://kang:audwkwkd@cluster0.t8kkn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('MongoDB Conected...'))
+  .catch((e) => console.log('MongoDB error: ', e));
+
+app.get('/', (req, res) => res.send('Hello, World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+//
