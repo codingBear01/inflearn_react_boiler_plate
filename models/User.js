27dff77dfbe55168dd58652b83b5c82316c3ef48
mameMan <jokingbear01@gmail.com>
2022-03-10 16:28:56 +0900

    authentification

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -52,6 +53,40 @@ userSchema.pre('save', function (next) {
     next();
   }
 });
+
+// 암호화되지 않은 plainPassword를 암호화하여와 DB내의 this.password 일치 여부 check
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatched) {
+    if (err) return callback(err);
+    callback(null, isMatched);
+  });
+};
+// jsonwebtoken package를 활용해 user token 생성
+userSchema.methods.generateToken = function (callback) {
+  let user = this;
+
+  const token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return callback(err);
+    callback(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, callback) {
+  let user = this;
+
+  // 토큰을 decode한다
+  jwt.verify(token, 'secretToken', function (err, decoded) {
+    // user._id를 이용해서 유저를 찾은 다음에
+    // clien에서 가져온 token과 DB에 보관된 토큰 일치 여부 check
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return callback(err);
+      callback(null, user);
+    });
+  });
+};
 
 // 위에 작성한 schema를 감쌀 model 생성
 const User = mongoose.model('User', userSchema);
